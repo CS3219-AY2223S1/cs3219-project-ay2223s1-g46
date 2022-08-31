e510932f0be3d470bf230e996de89118f92d274d
nighoggDatatype <hosherman1965@gmail.com>
2022-08-31 14:55:01 +0800

    Add basic socket.io code + (Line ending changes)

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
+import { Server } from "socket.io";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
@@ -13,5 +14,10 @@ app.get('/', (req, res) => {
 });
 
 const httpServer = createServer(app)
+
+const io = new Server(httpServer, { /* options */ });
+io.on("connection", (socket) => {
+    console.log("Logging socket connection")
+});
 
 httpServer.listen(8001);
