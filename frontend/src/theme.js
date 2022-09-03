d26f530f4f1d08672203f33c74b89c464457f9e3
Hazel Tan <e0424860@u.nus.edu>
2022-09-04 01:28:55 +0800

    Add customized theme files

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#8afcf2',
+      main: '#54c9bf',
+      dark: '#05988f',
+      contrastText: '#000000',
+    },
+  },
+})
+
+export default theme
