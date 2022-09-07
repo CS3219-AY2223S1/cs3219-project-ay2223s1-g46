b66d2825bd058551c7ee5e5b947e8a5d2517fcad
Hazel Tan <e0424860@u.nus.edu>
2022-09-08 00:38:42 +0800

    Fix repeated imports error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,8 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { ThemeProvider } from '@mui/system'
 import theme from './theme'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
