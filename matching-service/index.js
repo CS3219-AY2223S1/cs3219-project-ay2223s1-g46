834c7f48d98effbd236d564eec0c8b50ed5a3ec8
nighoggDatatype <hosherman1965@gmail.com>
2022-09-14 14:24:13 +0800

    Adjust room disconnect to use username

--- matching-service/index.js
+++ matching-service/index.js
@@ -22,6 +22,23 @@ const io = new Server(httpServer, { /* options */ });
 io.on("connection", (socket) => {
     console.log("Logging socket connection")
     socket.on("match", async (username, difficulty) => {
+        socket.on("disconnecting", (_) => {
+                for (const room of socket.rooms) {
+                    if (room !== socket.id) {
+                        socket.to(room).emit("user_leaves", username);
+                        socket.leave(room);
+                        //Future proof for rooms with more than 2 people
+                        const room_contents = io.sockets.adapter.rooms.get(room);
+                        const room_occupancy = room_contents.size;
+                        if (room_occupancy < 2) { 
+                            const lonely_socket_id = room_contents.values().next().value;
+                            const lonely_socket = io.sockets.sockets.get(lonely_socket_id);
+                            lonely_socket.leave(room);
+                            lonely_socket.emit("room_info", "Room has been torn down")
+                        }
+                    }
+                }
+            });
         console.log("Creating for: " + username + "; Difficulty: " + difficulty)
         var avaliableMatch = await ormAtomicFindFirstPendingMatchAndDelete(
             { difficulty: {$eq: difficulty} } //explict $eq to prevent injection attack
@@ -57,22 +74,7 @@ io.on("connection", (socket) => {
             }
         }
     });
-    socket.on("disconnecting", async (_) => { //TODO: Test this
-        for (const room of socket.rooms) {
-            if (room !== socket.id) {
-                socket.to(room).emit("user_leaves", socket.id);
-                socket.leave(room);
-                //Future proof for rooms with more than 2 people
-                const room_contents = io.sockets.adapter.rooms.get(room);
-                const room_occupancy = room_contents.size;
-                if (room_occupancy < 2) { 
-                    const lonely_socket_id = room_contents.values().next().value;
-                    const lonely_socket = io.sockets.sockets.get(lonely_socket_id);
-                    lonely_socket.leave(room); //TODO: Test this
-                    //TODO: Maybe add a message to the lonely socket about the room being torn down?
-                }
-            }
-        }
+    socket.on("disconnect", async (_) => {
         await ormAtomicFindFirstPendingMatchAndDelete( //TODO: Check this does not break if we find nothing
             { socket_id: {$eq: socket.id} } //explict $eq to prevent injection attack
         );
