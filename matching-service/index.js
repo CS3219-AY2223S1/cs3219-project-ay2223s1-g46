b739a757fd1d9f4598d30980310a22b8ae494b31
nighoggDatatype <hosherman1965@gmail.com>
2022-09-15 11:15:37 +0800

    Add room leave function and match idempotent logic

--- matching-service/index.js
+++ matching-service/index.js
@@ -22,23 +22,41 @@ const io = new Server(httpServer, { /* options */ });
 io.on("connection", (socket) => {
     console.log("Logging socket connection")
     socket.on("match", async (username, difficulty) => {
-        socket.on("disconnecting", (_) => {
-                for (const room of socket.rooms) {
-                    if (room !== socket.id) {
-                        socket.to(room).emit("user_leaves", username);
-                        socket.leave(room);
-                        //Future proof for rooms with more than 2 people
-                        const room_contents = io.sockets.adapter.rooms.get(room);
-                        const room_occupancy = room_contents.size;
-                        if (room_occupancy < 2) { 
-                            const lonely_socket_id = room_contents.values().next().value;
-                            const lonely_socket = io.sockets.sockets.get(lonely_socket_id);
-                            lonely_socket.leave(room);
-                            lonely_socket.emit("room_info", "Room has been torn down")
-                        }
+        for (const room of socket.rooms) {
+            if (room !== socket.id) {
+                console.log("Match request receive while already in room from socket " + socket.id)
+                return; //Already in room, ignore
+            }
+        }
+        //Flush existing timer, if any
+        var existing_match = await ormAtomicFindFirstPendingMatchAndDelete(
+            { socket_id: {$eq: socket.id} } //explict $eq to prevent injection attack
+        );
+        if (existing_match) {
+            clearTimeout(existing_match.timeout_id);
+        }
+        //Create unique disconnect/leave logic
+        socket.removeAllListeners("disconnecting");
+        const leaveRoomCallback = () => {
+            for (const room of socket.rooms) {
+                if (room !== socket.id) {
+                    socket.to(room).emit("user_leaves", username);
+                    socket.leave(room);
+                    //Future proof for rooms with more than 2 people
+                    const room_contents = io.sockets.adapter.rooms.get(room);
+                    const room_occupancy = room_contents.size;
+                    if (room_occupancy < 2) { 
+                        const lonely_socket_id = room_contents.values().next().value;
+                        const lonely_socket = io.sockets.sockets.get(lonely_socket_id);
+                        lonely_socket.leave(room);
+                        lonely_socket.emit("room_info", "Room has been torn down")
                     }
                 }
-            });
+            }
+        };
+        socket.on("disconnecting", (_) => {leaveRoomCallback();});
+        socket.on("leave_room", leaveRoomCallback);
+        
         console.log("Creating for: " + username + "; Difficulty: " + difficulty)
         var avaliableMatch = await ormAtomicFindFirstPendingMatchAndDelete(
             { difficulty: {$eq: difficulty} } //explict $eq to prevent injection attack
@@ -64,7 +82,6 @@ io.on("connection", (socket) => {
                 (msg) => socket.to(room_id).emit("message", msg)
             )
             socket.emit("match_user", avaliableMatch.username);
-            //TODO: Add mechanism to allow users to leave room without disconnecting
         } else {//Match not found
             try {
                 //Note: timeout_id doesn't work across worker threads
@@ -76,7 +93,6 @@ io.on("connection", (socket) => {
                         console.log("Timeout on socket " + socket.id)
                     }, 1000 * 30);
                 await ormCreatePendingMatch(username, socket.id, difficulty, timeout_id)
-                socket.removeAllListeners("match");
                 console.log("Match in progress")
                 socket.emit("match_result", "Match in progress");
             } catch (err) { //Catch block doesn't actually work. //TODO: Test this by submitting 2 names at the same time
