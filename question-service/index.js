36616f3983d8623f6c893cab8930e0e9b4f1dea1
Hazel Tan <e0424860@u.nus.edu>
2022-10-12 00:25:53 +0800

    Rename url after specific microservices and fix bug in create question for question service

--- question-service/index.js
+++ question-service/index.js
@@ -1,27 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { authorization } from './utils/middleware.js';
-import { createQuestion, getAllQuestions } from './controller/question-controller.js';
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { authorization } from './utils/middleware.js'
+import {
+  createQuestion,
+  getAllQuestions,
+} from './controller/question-controller.js'
 
-const app = express();
+const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-app.use(cookieParser());
-
+app.use(cookieParser())
 
 const questionRouter = express.Router()
-
 
 // Controller will contain all the User-defined Routes
 questionRouter.get('/', authorization, getAllQuestions)
 
 questionRouter.post('/', authorization, createQuestion)
-app.use('/api/question', questionRouter).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+app.use('/question-service/question', questionRouter).all((_, res) => {
+  res.setHeader('content-type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
-app.listen(8001, () => console.log('question-service listening on port 8001'));
+app.listen(8001, () => console.log('question-service listening on port 8001'))
--- user-service/index.js
+++ user-service/index.js
@@ -1,15 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { authorization, verifyTokenString } from './utils/middleware.js';
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { authorization, verifyTokenString } from './utils/middleware.js'
 
-const app = express();
+const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-app.use(cookieParser());
-import { createUser, deleteUser, logoutUser, updatePassword, loginUser, updateRole } from './controller/user-controller.js';
+app.use(cookieParser())
+import {
+  createUser,
+  deleteUser,
+  logoutUser,
+  updatePassword,
+  loginUser,
+  updateRole,
+} from './controller/user-controller.js'
 
 const userRouter = express.Router()
 
@@ -21,36 +28,33 @@ userRouter.put('/password', authorization, updatePassword)
 userRouter.delete('/delete', authorization, deleteUser)
 userRouter.put('/role', authorization, updateRole)
 
-app.use('/api/user', userRouter).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+app.use('/user-service/user', userRouter).all((_, res) => {
+  res.setHeader('content-type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
 const loginRouter = express.Router()
 loginRouter.post('/', loginUser)
-app.use('/api/login', loginRouter).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+app.use('/user-service/login', loginRouter).all((_, res) => {
+  res.setHeader('content-type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
 const logoutRouter = express.Router()
 logoutRouter.get('/', authorization, logoutUser)
-app.use('/api/logout', logoutRouter).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+app.use('/user-service/logout', logoutRouter).all((_, res) => {
+  res.setHeader('content-type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
 // for testing authorization
 const verifyRouter = express.Router()
 verifyRouter.post('/', verifyTokenString, (req, res) => {
-    return res.json({user: {username: req.username, role: req.role}});
-});
-app.use('/api/verify', verifyRouter).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+  return res.json({ user: { username: req.username, role: req.role } })
+})
+app.use('/user-service/verify', verifyRouter).all((_, res) => {
+  res.setHeader('content-type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
-
-
-
-app.listen(8000, () => console.log('user-service listening on port 8000'));
+app.listen(8000, () => console.log('user-service listening on port 8000'))
